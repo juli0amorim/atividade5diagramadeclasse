e1c9fb7db242cf7d7cb5539ebac98b94b1bd2b13
Julio Henrique Amorim Silva <julio2005.henrique@gmail.com>
2024-03-27 21:00:48 -0300

    Commit incial

--- atividade_avaliativa_02/nota_zeroadez/src/nota_zeroadez.java
+++ atividade_avaliativa_02/nota_zeroadez/src/nota_zeroadez.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class App {
+public class nota_zeroadez {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double nota; 
--- atividade_avaliativa_02/usuario_senha/src/usuario_senha.java
+++ atividade_avaliativa_02/usuario_senha/src/usuario_senha.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class App {
+public class usuario_senha {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         String nomeUsuario, senha;
